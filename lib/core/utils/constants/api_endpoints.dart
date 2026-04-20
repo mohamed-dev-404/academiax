69dcca3542e523059f98a8c4a93916c4e7962937
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-04-20 14:34:19 +0200

    feat(assignment): define API endpoints for assignment management and student submissions.

--- lib/core/utils/constants/api_endpoints.dart
+++ lib/core/utils/constants/api_endpoints.dart
@@ -30,4 +30,24 @@ class EndPoints {
   static const String updateProfile = 'users/profile';
   static const String createUploadUrl = 'users/profile-picture/presigned-url';
   static const String saveProfilePic = 'users/profile-picture';
+
+
+  //? --- Assignments --- ;
+  
+  //* S3 Helpers
+  static String createAssignmentUploadUrls(String courseId) => 'courses/$courseId/context/presigned-urls';
+
+  //* Management (Instructor)
+  static String addAssignment(String courseId) => 'instructor/courses/$courseId/assignments';
+  static String deleteAssignment(String assignmentId) => 'instructor/assignments/$assignmentId';
+  static String deleteAssignmentItem(String assignmentId) => 'instructor/assignments/$assignmentId/items';
+  static String getCourseAssignments(String courseId) => 'courses/$courseId/assignments';
+  static String getAssignmentDetails(String assignmentId) => 'assignments/$assignmentId';
+
+  //? --- Submissions --- ;
+  static String getSubmissions(String assignmentId) => 'assignments/$assignmentId/submissions';
+  static String getSubmissionDetails(String submissionId) => 'assignments/submissions/$submissionId';
+  static String gradeSubmission(String submissionId) => 'assignments/submissions/$submissionId/grade';
+  static String approveSubmissions(String assignmentId) => 'assignments/$assignmentId/approve-submissions';
+      
 }
